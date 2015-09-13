222fe9cb5b825762e2f0bbdc2cdf6d24d8edd2eb
Faure Hu Chavez <faurehu@gmail.com>
2015-09-13 23:11:33 +0100

    Rearranged code and made ES6 compliant

---
+++ config/init.js
@@ -0,0 +1,15 @@
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import express from 'express';
+
+module.exports = (app) => {
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+}
