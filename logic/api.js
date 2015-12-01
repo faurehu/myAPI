cf325c628ad7b4d7d1aee3d8a1837a9156f8fc93
faure <hi@faure.hu>
2015-12-01 01:04:48 +0000

    Memoization works

--- logic/api.js
+++ logic/api.js
@@ -3,43 +3,25 @@ import request from 'request';
 import https from 'https';
 let keys = config().keys;
 
-module.exports = (app) => {
-  return {
-    getBlog: (req, res, next) => {
+let store = {};
+let timeCheck = 0;
 
-      let handleError = (err) => { res.status(500); return next(err); };
+let memoCheck = (key, res) => {
+  let handleError = (err) => { res.status(500); return next(err); };
+  let success = () => { timeCheck = Date.now(); res.json(store[key]); };
+  if (Date.now() > timeCheck + ( 5 * 60 * 1000 )) {
+    callAPIs(res, store, timeCheck).then(success).catch(handleError);
+  } else { success(); }
+}
 
-      let response = (data) => {
-        res.json(data.map((node) => {
-          return node.dataValues;
-        }));
+let callAPIs = (res) => {
+  return new Promise((resolve, reject) => {
+    let twitterAPI = new Promise((resolve, reject) => {
+      var options = {
+        hostname: 'api.twitter.com',
+        path: 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=faurehu&count=50&include_rts=true',
+        headers: {Authorization: 'Bearer ' + global.twitterToken}
       }
-
-      app.get('models').post.findAll({order: '"createdAt" DESC'})
-      .then(response).catch(handleError);
-    },
-    getImages: (req, res, next) => {
-
-      let handleError = (err) => { res.status(500); return next(err); };
-
-      let response = (data) => {
-        res.json(data.map((node) => {
-          return node.dataValues;
-        }));
-      }
-
-      app.get('models').Image.findAll()
-      .then(response).catch(handleError);
-    },
-    getTwitter: (req, res, next) => {
-
-      let handleError = (err) => { res.status(500); return next(err); };
-
-      var options = { hostname: 'api.twitter.com',
-                      path: 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=faurehu&count=50&include_rts=true',
-                      headers: {Authorization: 'Bearer ' + global.twitterToken}
-                    }
-
       https.get(options, (result) => {
         var buffer = '';
         result.setEncoding('utf8');
@@ -48,21 +30,52 @@ module.exports = (app) => {
         });
         result.on('end', () => {
           var response = JSON.parse(buffer);
-          res.json(response.map((tweet) => {
+          store.tweets = response.map((tweet) => {
             return {
               myText: tweet.text,
               imgUrl: tweet.entities.media && tweet.entities.media[0].media_url,
               author: tweet.retweeted_status && tweet.retweeted_status.user.screen_name,
               text: tweet.retweeted_status && tweet.retweeted_status.text
             }
-          }));
+          });
+          resolve();
         });
-      }).on('error', handleError);
+      }).on('error', reject);
+    });
+    let promises = [twitterAPI];
+    Promise.all(promises).then(resolve).catch(reject);
+  });
+}
+
+module.exports = (app) => {
+  return {
+    getBlog: (req, res, next) => {
+      let handleError = (err) => { res.status(500); return next(err); };
+      let response = (data) => {
+        res.json(data.map((node) => {
+          return node.dataValues;
+        }));
+      }
+      app.get('models').post.findAll({order: '"createdAt" DESC'})
+      .then(response)
+      .catch(handleError);
+    },
+    getImages: (req, res, next) => {
+      let handleError = (err) => { res.status(500); return next(err); };
+      let response = (data) => {
+        res.json(data.map((node) => {
+          return node.dataValues;
+        }));
+      }
+      app.get('models').Image.findAll()
+      .then(response)
+      .catch(handleError);
+    },
+    getTwitter: (req, res, next) => {
+      memoCheck("tweets", res);
     },
     getGithub: (req, res, next) => {
-
       let handleError = (err) => { res.status(500); return next(err); };
-
       let response = (data) => {
         let options = {
           url: `https://api.github.com/user/repos?access_token=${data.dataValues.token}&visibility=public&affiliation=owner,collaborator&sort=updated`,
