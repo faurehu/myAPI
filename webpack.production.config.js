7d641f98fa919757570735a324965eddd9f8ce87
Faure Hu <hi@faure.hu>
2016-07-30 00:39:00 +0100

    Alias for dist/react

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -10,7 +10,11 @@ module.exports = {
     filename: "bundle.js"
   },
   resolve: {
-    extensions: ["", ".js", ".jsx", ".js.jsx"]
+    extensions: ["", ".js", ".jsx", ".js.jsx"],
+    alias: {
+       'react$': path.join(__dirname, 'node_modules', 'react','dist','react.min.js'),
+       'react-dom$': path.join(__dirname, 'node_modules', 'react-dom','dist','react-dom.min.js')
+    }
   },
   module: {
     loaders: [
